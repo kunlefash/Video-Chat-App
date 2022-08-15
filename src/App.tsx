cbc113952db658db3707f7e43807802dbffde4f9
kunlefash <kunlefashmayop@gmail.com>
2022-08-15 19:19:11 -0300

    Adding video streaming

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import AgoraUIKit, { PropsInterface } from "agora-react-uikit";
+import AgoraUIKit, { PropsInterface, layout } from "agora-react-uikit";
 
 const App: React.FunctionComponent = () => {
   const [videocall, setVideocall] = useState(true);
+  const [isHost, setHost] = useState(false)
+  const [isPinned, setPinned] = useState(false)
   const props: PropsInterface = {
     rtcProps: {
       appId: "<Your Agora App ID>",
       channel: "test",
+      role: isHost ? 'host' : 'audience',
+      layout: isPinned ? layout.pin : layout.grid,
       token: null, // pass in channel token if the app is in secure mode
     },
     callbacks: {
@@ -18,13 +22,29 @@ const App: React.FunctionComponent = () => {
   };
   return (
     <div style={styles.container}>
-      {videocall ? (
-        <AgoraUIKit rtcProps={props.rtcProps} callbacks={props.callbacks} />
-      ) : null}
-    </div>
-  );
-};
+        {videocall ? (<>
+          <div style={styles.nav}>
+            <p style={{ fontSize: 20, width: 200 }}>You're {isHost ? 'a host' : 'an audience'}</p>
+            <p style={styles.btn} onClick={() => setRole(!isHost)}>Change Role</p>
+            <p style={styles.btn} onClick={() => setPinned(!isPinned)}>Change Layout</p>
+          </div>
+          <AgoraUIKit
+            rtcProps={props.rtcProps}
+            callbacks={props.callbacks}
+            styleProps={props.styleProps} /></>
+        ) : (
+          <h3 style={styles.btn} onClick={() => setVideocall(true)}>Start Call</h3>
+        )}
+      </div>
+  )
+}
 
 const styles = {
-  container: { width: "100vw", height: "100vh", display: "flex", flex: 1 },
-};
+  container: { width: '100vw', height: '100vh', display: 'flex', flex: 1, backgroundColor: '#007bff22'},
+  heading: { textAlign: 'center' as const, marginBottom: 0 },
+  videoContainer: { display: 'flex', flexDirection: 'column', flex: 1 } as React.CSSProperties,
+  nav: { display: 'flex', justifyContent: 'space-around' },
+  btn: { backgroundColor: '#007bff', cursor: 'pointer', borderRadius: 5, padding: 5, color: '#ffffff', fontSize: 20 },
+}
+
+export default App
